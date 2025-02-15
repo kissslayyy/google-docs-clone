0ee8b0815b0bb0803106085531bff6de0ab6fb61
kissslayyy <kislaygupta007@gmail.com>
2025-02-15 11:10:58 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://first-gibbon-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
